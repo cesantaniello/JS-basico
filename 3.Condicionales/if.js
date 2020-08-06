a9ff23128def2f2a37691d689ae2162d19f00ae7
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2020-08-07 00:22:28 +0200

    3.1-if

---
+++ 3.Condicionales/if.js
@@ -0,0 +1,14 @@
+var edad = 17;
+if (edad === 18){
+  console.log("Puedes votar");
+}else if (edad > 18){
+  console.log("Puedes votar de nuevo");
+}else {
+  console.log("Aún no puedes votar");
+}
+
+//Ternario
+
+var numero = 1;
+
+var resultado = numero === 1 ? "Sí soy un uno" : "No, no soy un uno";
