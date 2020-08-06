cec1455478210244af64f4f494e92546047588fd
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2020-08-06 21:37:09 +0200

    1.4-variables

---
+++ variables.js
@@ -0,0 +1,16 @@
+var nombre = "Carlos";
+
+//declaración de variable
+var edad;
+
+//inicialización de variable
+edad = 37;
+
+var elementos = ["Computadora","Celular"];
+
+var persona = {
+  nombre: "Carlos",
+  edad: 37
+}
+
+persona;
