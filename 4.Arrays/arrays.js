77ab6a2852721ec571071947c5fdb8249f452207
Carlos <carlos.e.santaniello@gmail.com>
2020-11-14 16:19:54 +0100

    Arrays

---
+++ 4.Arrays/arrays.js
@@ -0,0 +1,23 @@
+var frutas = ["Manzana", "Plátano", "Cereza", "Fresa"];
+console.log(frutas);
+
+var masFrutas = frutas.push("Uvas");
+console.log(frutas);
+
+var ultimo = frutas.pop("Uvas");
+console.log(frutas);
+
+var nuevaLongitud = frutas.unshift("Uvas");
+console.log(frutas);
+
+var borrarFruta = frutas.shift("Uvas");
+console.log(frutas);
+
+var nuevaLongitud = frutas.unshift("Uvas");
+console.log(frutas);
+
+var borrarFruta = frutas.shift("Uvas");
+console.log(frutas);
+
+var posicion = frutas.indexOf("Cereza");
+console.log(frutas);
